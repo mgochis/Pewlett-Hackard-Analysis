934171b1576fd292e2c97ee70512360db53601b4
Michael Gochis <mgochis@Michaels-MacBook-Pro.local>
2022-07-01 23:45:12 -0600

    before readme again

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -21,3 +21,14 @@ INTO retiring_titles
 FROM retirement_tables_active
 GROUP BY title
 ORDER BY count DESC;
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, d.from_date, d.to_date, t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE t.to_date = '9999-01-01'
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
